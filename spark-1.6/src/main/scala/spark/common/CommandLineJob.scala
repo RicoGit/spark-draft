a1931be44f652c558fc0c13eb1db5c2940263069
Constantine Solovev <constantine.solovev@gmail.com>
2017-08-17 10:47:14 +0400

    init commit

---
+++ spark-1.6/src/main/scala/spark/common/CommandLineJob.scala
@@ -0,0 +1,32 @@
+package spark.common
+
+import com.typesafe.scalalogging.slf4j.Logger
+import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.LoggerFactory
+
+
+/** Base parent for all spark spark.job. */
+trait CommandLineJob {
+
+  protected val logger = Logger(LoggerFactory.getLogger(this.getClass.getName))
+
+  def main(args: Array[String]) = {
+    logger.info(s"start with arguments: ${args.mkString(",")}")
+    val sc: SparkContext = createSparkContext
+    try {
+      doWork(sc, args)
+    } finally {
+      sc.stop()
+    }
+  }
+
+  /**
+   * Primary method. Override this method and put all logic here
+   */
+  def doWork(sc: SparkContext, args: Array[String])
+
+  def sparkConf = new SparkConf()
+
+  protected def createSparkContext = new SparkContext(sparkConf)
+
+}
---
+++ spark-1.6/src/main/scala/spark/common/SimpleJob.scala
@@ -0,0 +1,34 @@
+package spark.common
+
+import com.typesafe.scalalogging.slf4j.Logger
+import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.LoggerFactory
+
+/** Base parent for all spark spark.job. */
+trait SimpleJob {
+
+  protected val jobName = this.getClass.getSimpleName.replace("$", "")
+  protected val logger = Logger(LoggerFactory.getLogger(this.getClass.getName))
+
+  def main(args: Array[String]) = {
+
+    logger.info("Job starts")
+    // todo change
+    val sc: SparkContext = createSparkContext
+    try {
+      doWork(sc)
+    } finally {
+      sc.stop()
+    }
+  }
+
+  /**
+   * Primary method. Override this method and put all logic here
+   */
+  protected def doWork(sc: SparkContext)
+
+  protected def sparkConf = new SparkConf()
+
+  protected def createSparkContext = new SparkContext(sparkConf)
+
+}
